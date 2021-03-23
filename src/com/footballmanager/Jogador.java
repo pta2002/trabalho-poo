131549b483965a457c5d14054c1cf376ccb21030
Pedro Alves <pta2002@pta2002.com>
2021-03-23 11:43:03 +0000

    Criar classe Jogador
    Co-authored-by: Matilde Bravo <matildeopbravo@gmail.com>
    Co-authored-by: Henrique Parola <parolajc@hotmail.com>

---
+++ src/com/footballmanager/Jogador.java
@@ -0,0 +1,14 @@
+package com.footballmanager;
+
+public class Jogador {
+    private String nome;
+
+    private double velocidade;
+    private double resistencia;
+    private double destreza;
+    private double impulsao;
+    private double jogoDeCabeca;
+    private double remate;
+    private double capacidadeDePasse;
+    private double habilidade;
+}
